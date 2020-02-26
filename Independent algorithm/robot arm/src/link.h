d6928570daea851e9bb9fff91ccd8e79d514877d
Arsenic <arsenic1026@gmail.com>
2020-02-26 21:05:00 +0900

    adjustment

---
+++ Independent algorithm/robot arm/src/link.h
@@ -0,0 +1,26 @@
+/*
+ * link.h
+ *
+ *  Created on: 2020/2/26
+ *      Author: Arsenic
+ */
+
+#ifndef LINK_H_
+#define LINK_H_
+
+typedef struct {
+    /***parameter***/
+    //ID
+    uint8_t myID;
+    uint8_t brotherID;
+    uint8_t childID;
+    uint8_t motherID;
+
+    //TODO add eigen lib
+
+    /***parameter***/
+
+} link_t;
+
+
+#endif /* LINK_H_ */
