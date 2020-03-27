e22e1d94561f493a09cb42011c4e99aecbe68ec9
Arsenic <arsenic1026@gmail.com>
2020-03-27 16:02:03 +0900

    add make Tx data and fix bit shift bug

--- Independent algorithm/UART-manager/UARTManager.c
+++ Independent algorithm/UART-manager/UARTManager.c
@@ -6,16 +6,22 @@
  */
 #include "UARTManager.h"
 
+void initUARTManager(UARTManager_t *manager) {
+    manager->onCapture = 0;
+    manager->dataPointer = 0;
+    manager->headerPointer = 0;
+}
+
 void rxUpdate(UARTManager_t *manager, uint8_t d) {
     if (!(manager->onCapture)) {    // try detect header packet
         manager->headerDetector[manager->headerPointer] = d;
 
         if (    //check header packet
-            (manager->headerDetector[((manager->headerPointer)+1)%4] == HEADER_PACKET[0]) &&
-            (manager->headerDetector[((manager->headerPointer)+2)%4] == HEADER_PACKET[1]) &&
-            (manager->headerDetector[((manager->headerPointer)+3)%4] == HEADER_PACKET[2]) &&
-            (manager->headerDetector[((manager->headerPointer))%4] == HEADER_PACKET[3])
-        ) {     //stert capture
+            (manager->headerDetector[((manager->headerPointer) + 1)%4] == HEADER_PACKET[0]) &&
+            (manager->headerDetector[((manager->headerPointer) + 2)%4] == HEADER_PACKET[1]) &&
+            (manager->headerDetector[((manager->headerPointer) + 3)%4] == HEADER_PACKET[2]) &&
+            (manager->headerDetector[((manager->headerPointer) + 0)%4] == HEADER_PACKET[3])
+        ) {     //start capture
             manager->onCapture = 1;
             manager->dataPointer = 0;
         }
@@ -30,15 +36,15 @@ void rxUpdate(UARTManager_t *manager, uint8_t d) {
 
             if (d == STOP_PACKET) {     //save available rx data
                 for (int i = 0; i < RX_DATA_LENGTH; i++) {
-                    manager->data[i] = (float)(
-                        manager->serializedRxData[4*i] |
-                        manager->serializedRxData[4*i + 1] |
-                        manager->serializedRxData[4*i + 2] |
-                        manager->serializedRxData[4*i + 3]
+                    manager->rxData[i] = (float)(
+                        (manager->serializedRxData[4*i + 0] << 24) |
+                        (manager->serializedRxData[4*i + 1] << 16) |
+                        (manager->serializedRxData[4*i + 2] << 8) |
+                         manager->serializedRxData[4*i + 3]
                     );
                 }
             }
-            
+
         } else {
             manager->serializedRxData[manager->dataPointer] = d;
             manager->dataPointer++;
@@ -52,3 +58,18 @@ void rxUpdateN(UARTManager_t *manager, uint8_t *d, int n) {
     }
 }
 
+void makeTxData(UARTManager_t *manager) {
+    for (int i = 0; i < 4; i++) {               //make header packet
+        manager->serializedTxData[i] = HEADER_PACKET[i];
+    }
+
+    for (int i; i < TX_DATA_LENGTH; i++) {      //make data packet
+        manager->serializedTxData[4 + (4*i) + 0] = (uint8_t)((manager->txData[i] & 0xFF000000) >> 24);
+        manager->serializedTxData[4 + (4*i) + 1] = (uint8_t)((manager->txData[i] & 0x00FF0000) >> 16);
+        manager->serializedTxData[4 + (4*i) + 2] = (uint8_t)((manager->txData[i] & 0x0000FF00) >> 8);
+        manager->serializedTxData[4 + (4*i) + 3] = (uint8_t)((manager->txData[i] & 0x000000FF) >> 0);
+    }
+
+    //make stop packet
+    manager->serializedTxData[4 + 4*TX_DATA_LENGTH] = STOP_PACKET;
+}
--- Independent algorithm/UART-manager/UARTManager.h
+++ Independent algorithm/UART-manager/UARTManager.h
@@ -14,20 +14,25 @@ const uint8_t HEADER_PACKET[] = {'s', 0xFF, 0x00, 0xAA};
 const uint8_t STOP_PACKET = 'e';
 
 typedef struct {
-    uint8_t onCapture = 0;
+    uint8_t onCapture;
 
-    uint16_t dataPointer = 0;
-    uint8_t headerPointer = 0;
+    uint16_t dataPointer;
+    uint8_t headerPointer;
 
     uint8_t headerDetector[4];
 
     uint8_t serializedRxData[4*RX_DATA_LENGTH];
-    uint8_t serializedTxData[4*TX_DATA_LENGTH];
-    
-    float data[RX_DATA_LENGTH];    
-}UARTManager_t;
+    uint8_t serializedTxData[4 + 4*TX_DATA_LENGTH + 1];
+
+    float rxData[RX_DATA_LENGTH];  
+    float txData[TX_DATA_LENGTH];
+} UARTManager_t;
+
+void initUARTManager(UARTManager_t *manager);
 
 void rxUpdate(UARTManager_t *manager, uint8_t d);
 void rxUpdateN(UARTManager_t *manager, uint8_t *d, int n);
+
+void makeTxData(UARTManager_t *manager);
 
 #endif /* UARTMANAGER_H_ */
